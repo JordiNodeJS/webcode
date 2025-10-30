f0dbb1cacd4a103dbad73ebe0f0d491d16c21e3a
JordiNodejs <jordi.nodejs@gmail.com>
2025-10-30 21:13:06 +0100

    fix(layout): change default theme to dark for improved user experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -179,7 +179,7 @@ export default function RootLayout({
         >
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
